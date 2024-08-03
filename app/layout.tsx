d3e2831bbd572d5d49948f41142c15f5d26d37e1
adamrichardturner <hello@adamrichardturner.dev>
2024-08-03 19:02:27 +0100

    Add og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,35 @@
-import type { Metadata } from "next";
-import { League_Spartan } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
-import { Toaster } from "@/components/ui/sonner";
+import type { Metadata } from "next"
+import { League_Spartan } from "next/font/google"
+import "./globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/sonner"
 
 const spartan = League_Spartan({
   subsets: ["latin"],
   weight: ["400", "500", "700"],
-});
+})
 
 export const metadata: Metadata = {
   title: "Invoice Tracker | Adam Richard Turner",
-  description: "An Invoice Tracking app built with Next & Express",
-};
+  description: "Invoice Tracking app built with Next & Express",
+  openGraph: {
+    title: "Invoice Tracker | Adam Richard Turner",
+    description: "Invoice Tracking app built with Next & Express",
+    images: "https://invoice-tracker.adamrichardturner.dev/opengraph-image.png",
+  },
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang='en' suppressHydrationWarning>
       <body className={spartan.className}>
         <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
+          attribute='class'
+          defaultTheme='system'
           enableSystem
           disableTransitionOnChange
         >
@@ -33,5 +38,5 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }

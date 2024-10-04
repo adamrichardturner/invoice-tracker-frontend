521deb42ed42754c1eef539bf10431b314c3d8e2
adamrichardturner <hello@adamrichardturner.dev>
2024-10-04 23:13:38 +0100

    Login user with demo

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,16 +1,13 @@
-"use client";
+"use client"
 
-import LoginForm from "@/components/Auth/Login";
-import useUser from "@/hooks/user/useUser";
+import LoginForm from "@/components/Auth/Login"
+import useUser from "@/hooks/user/useUser"
 
 export default function Page() {
-  const { loginWithPassword, loginWithDemoAccount } = useUser();
+  const { loginWithDemoAccount } = useUser()
   return (
-    <div className="flex items-center px-2 justify-center min-h-screen bg-background">
-      <LoginForm
-        onLogin={loginWithPassword}
-        loginWithDemoAccount={loginWithDemoAccount}
-      />
+    <div className='flex items-center px-2 justify-center min-h-screen bg-background'>
+      <LoginForm loginWithDemoAccount={loginWithDemoAccount} />
     </div>
-  );
+  )
 }
--- components/Auth/Login.tsx
+++ components/Auth/Login.tsx
@@ -1,122 +1,46 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
-import { FaWrench } from "react-icons/fa";
-import Link from "next/link";
-
-interface LoginFormProps {
-  onLogin: (email: string, password: string) => void;
-  loginWithDemoAccount: () => void;
-}
-
-const schema = z.object({
-  email: z.string().email("Invalid email address"),
-  password: z.string().min(6, "Password must be at least 6 characters long"),
-});
-
-type LoginFormData = z.infer<typeof schema>;
+import React from "react"
+import { Button } from "@/components/ui/button"
+import { FaWrench } from "react-icons/fa"
 
 export default function LoginForm({
-  onLogin,
   loginWithDemoAccount,
-}: LoginFormProps) {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<LoginFormData>({
-    resolver: zodResolver(schema),
-  });
-
-  const onSubmit = (data: LoginFormData) => {
-    onLogin(data.email, data.password);
-  };
-
+}: {
+  loginWithDemoAccount: () => void
+}) {
   return (
-    <div className="max-w-full md:w-[480px] space-y-10 py-10 bg-foreground p-8 rounded-lg shadow-lg">
-      <div className="space-y-2 text-center">
-        <div className="flex flex-col justify-center items-center">
+    <div className='max-w-full md:w-[480px] space-y-10 py-10 bg-foreground p-8 rounded-lg shadow-lg'>
+      <div className='space-y-2 text-center'>
+        <div className='flex flex-col justify-center items-center'>
           <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="40"
-            height="38"
-            viewBox="0 0 40 38"
+            xmlns='http://www.w3.org/2000/svg'
+            width='40'
+            height='38'
+            viewBox='0 0 40 38'
           >
             <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M10.6942 0.291992L20 18.9998L29.3058 0.291992C35.6645 3.64055 40 10.3139 40 17.9998C40 29.0454 31.0457 37.9998 20 37.9998C8.9543 37.9998 0 29.0454 0 17.9998C0 10.3139 4.33546 3.64055 10.6942 0.291992Z"
-              fill="#7C5DFA"
+              fillRule='evenodd'
+              clipRule='evenodd'
+              d='M10.6942 0.291992L20 18.9998L29.3058 0.291992C35.6645 3.64055 40 10.3139 40 17.9998C40 29.0454 31.0457 37.9998 20 37.9998C8.9543 37.9998 0 29.0454 0 17.9998C0 10.3139 4.33546 3.64055 10.6942 0.291992Z'
+              fill='#7C5DFA'
             />
           </svg>
-          <h1 className="text-3xl font-bold text-heading">Invoicez</h1>
+          <h1 className='text-3xl font-bold text-heading'>Invoicez</h1>
         </div>
 
-        <p className="text-body text-sm leading-tight">
-          Enter your email and password below or login as a demo user.
-        </p>
+        <h2 className='text-body text-lg leading-tight'>
+          Try out Invoicez with a demo account.
+        </h2>
       </div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="space-y-6">
-          <div className="space-y-2 text-heading">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              className="bg-foreground border dark:border-white"
-              id="email"
-              type="email"
-              {...register("email")}
-              required
-            />
-            {errors.email && (
-              <p className="text-red-500 text-sm">{errors.email.message}</p>
-            )}
-          </div>
-          <div className="space-y-2 text-heading">
-            <Label htmlFor="password">Password</Label>
-            <Input
-              className="bg-foreground dark:border-white"
-              id="password"
-              type="password"
-              {...register("password")}
-              required
-            />
-            {errors.password && (
-              <p className="text-red-500 text-sm">{errors.password.message}</p>
-            )}
-          </div>
-          <Button
-            type="submit"
-            className="w-full mt-6 text-white bg-primary rounded-roundedBtn"
-          >
-            Sign In
-          </Button>
-        </div>
-      </form>
-      <Separator className="my-6 border-primary-foreground" />
-      <div className="space-y-4 text-heading dark:text-secondaryBody">
+      <div className='space-y-4 text-heading dark:text-secondaryBody'>
         <Button
-          variant="outline"
-          className="w-full bg-foreground dark:bg-white dark:text-black dark:hover:text-black rounded-roundedBtn"
+          variant='outline'
+          className='w-full py-8 bg-foreground dark:bg-white dark:text-black dark:hover:text-black rounded-roundedBtn'
           onClick={loginWithDemoAccount}
         >
-          <FaWrench className="h-5 w-5 mr-2" />
-          <span className="pt-[3px]">Login as Demo User</span>
+          <FaWrench className='h-5 w-5 mr-2' />
+          <span className='pt-[3px] text-lg'>Demo Login</span>
         </Button>
       </div>
-      <Separator className="my-8" />
-      <div className="text-center">
-        <p className="text-heading dark:text-secondaryBody text-sm">
-          Don't have an account?{" "}
-          <Link href="/auth/register" className="underline" prefetch={false}>
-            Register
-          </Link>
-        </p>
-      </div>
     </div>
-  );
+  )
 }

e9c1b4e63079380af91bb74c9f2b4da3461f4156
adamrichardturner <hello@adamrichardturner.dev>
2024-11-16 00:45:25 +0000

    Very simple auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,28 @@
-"use client"
+"use client";
 
-import InvoiceDisplay from "@/components/InvoiceDisplay/InvoiceDisplay"
-import useInvoices from "@/hooks/invoices/useInvoices"
-import useFilteredInvoices from "../hooks/invoices/useFilteredInvoices"
-import { useEffect } from "react"
-import { useRouter } from "next/navigation"
+import InvoiceDisplay from "@/components/InvoiceDisplay/InvoiceDisplay";
+import useInvoices from "@/hooks/invoices/useInvoices";
+import useFilteredInvoices from "../hooks/invoices/useFilteredInvoices";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function InvoicesPage() {
-  const router = useRouter()
-  const { loading, invoicesLoaded, fetchInvoices } = useInvoices()
-  const { filteredInvoices } = useFilteredInvoices()
+  const router = useRouter();
+  const { loading, invoicesLoaded, fetchInvoices } = useInvoices();
+  const { filteredInvoices } = useFilteredInvoices();
 
   useEffect(() => {
-    const token = localStorage.getItem("token")
+    const token = localStorage.getItem("token");
     if (!token) {
-      router.push("/auth/demo")
-      return
+      router.push("/auth/demo");
+      return;
     }
-    fetchInvoices()
-  }, [router, fetchInvoices])
+    fetchInvoices();
+  }, [router, fetchInvoices]);
 
   return (
-    <div className='flex min-h-screen w-full items-start justify-center pt-[120px] md:pl-[103px] md:pt-[77px]'>
-      <main className='flex flex-col h-full w-full md:w-[768px] items-center justify-center mx-4'>
+    <div className="flex min-h-screen w-full items-start justify-center pt-[120px] md:pl-[103px] md:pt-[77px]">
+      <main className="flex flex-col h-full w-full md:w-[768px] items-center justify-center mx-4">
         <InvoiceDisplay
           filteredInvoices={filteredInvoices}
           invoicesLoaded={invoicesLoaded}
@@ -30,5 +30,5 @@ export default function InvoicesPage() {
         />
       </main>
     </div>
-  )
+  );
 }
